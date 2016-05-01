299d7942aafb0c5620cebc0452e45d8b8ca6422c
test <test@rayjoy.com>
2016-05-01 10:30:48 +0800

    up

--- hdu1001.c
+++ hdu1001.c
@@ -1,12 +1,17 @@
 #include<stdio.h>
 int main()
 {
-		int a;
-		int b;
-		while(scanf("%d %d",&a,&b)!=EOF)
-		{
-				printf("%d\n",a+b);
-		}
-
-		return 0;
+	int a=0;
+	int sum=0;
+	int i=0;
+	while( scanf("%d",&a)!=EOF)
+	{
+			sum=0;
+			for(i=1;i<=a;i++)
+			{
+					sum+=i;
+			}
+			printf("%d\n\n",sum);
+	}
+	return 0;
 }
